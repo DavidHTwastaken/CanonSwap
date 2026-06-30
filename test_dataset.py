3bafeea09b710fbdb4b78570ac8f686360b7ce67
David Houle-Tymeczko <david.houletymeczko@ontariotechu.net>
2026-06-30 16:37:09 -0400

    Sort images and videos so the order is consistent

--- test_dataset.py
+++ test_dataset.py
@@ -55,11 +55,11 @@ output_dir = 'results'
 # m = pd.read_csv(os.path.join(root, 'map.csv'), header=0)
 # print(m)
 vid_data_dir = os.path.join(root, 'targets')
-videos = list(os.path.join(vid_data_dir, v)
-              for v in os.listdir(vid_data_dir) if v.endswith('.mp4'))
+videos = list(sorted(os.path.join(vid_data_dir, v)
+              for v in os.listdir(vid_data_dir) if v.endswith('.mp4')))
 img_data_dir = os.path.join(root, 'sources')
-images = list(os.path.join(img_data_dir, i)
-              for i in os.listdir(img_data_dir) if i.lower().endswith('jpg'))
+images = list(sorted(os.path.join(img_data_dir, i)
+              for i in os.listdir(img_data_dir) if i.lower().endswith('jpg')))
 image_csv = pd.read_csv(os.path.join(img_data_dir, 'identities.csv'))
 # # preprocess each video and image
 # videos = m['file'][m['is_video'] == 1]
